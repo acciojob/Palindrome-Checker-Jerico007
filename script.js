84dfcd88242ae5bdf9e9b6e8df9d3bfabc7d5811
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-06-26 13:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+ let j = str.length - 1;
+ let i = 0;
 
+	while(i < j)
+		{
+			if(str[i] != str[j])
+			{
+				return false;
+			}
+			i++;
+			j--;
+		}
+
+	return true;
+	
 }
 module.exports = palindrome
